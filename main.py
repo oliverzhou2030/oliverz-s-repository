c802657e9fa6aca802e60be205aeaa63dfa09c7b
OliverZ <oliver.zhou.2030@gmail.com>
2025-07-31 12:34:06 -0700

    1

---
+++ main.py
@@ -0,0 +1,29 @@
+
+#!/usr/bin/python3
+
+import jetson_inference
+import jetson_utils
+
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", type=str, help="filename of the image to process")
+parser.add_argument("--network", type=str, default="resnet-18", help="model to use, can be:  googlenet, resnet-18, ect. (see --help for others)")
+opt = parser.parse_args()
+
+img = jetson_utils.loadImage(opt.filename)
+
+net = jetson_inference.imageNet(
+    opt.network,
+    model="resnet18.onnx",
+    input_blob="input_0",
+    output_blob="output_0",
+    labels="labels.txt"
+)
+
+class_idx, confidence = net.Classify(img)
+
+class_desc = net.GetClassDesc(class_idx)
+
+print("image is recognized as "+ str(class_desc) +" (class #"+ str(class_idx) +") with " + str(confidence*100)+"% confidence")
